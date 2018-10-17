b7e60263637f80cf43c77c558ba134c684c8acfa
Ashiscodebat <44222051+Ashiscodebat@users.noreply.github.com>
2018-10-17 13:27:48 +0530

    Update stringrev.py

--- stringrev.py
+++ stringrev.py
@@ -2,7 +2,11 @@ st = input()
 l = []
 rev = []
 for i in range(0,len(st)):
-    l.append(st[i])
+    if((st[i] <= '90' and st[i] >= '65' ) or (st[i] >= '97' and st[i] <= '112')):
+        l.append(st[i])
+    else:
+        print("Invalid input")
+        exit(1)
 i = len(l)-1
 while(i >= 0):
     rev.append(l[i])
