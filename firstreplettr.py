cd9760324a033b50101890119e035d2089d9636c
Ashiscodebat <44222051+Ashiscodebat@users.noreply.github.com>
2018-10-20 22:08:09 +0530

    Update firstreplettr.py

--- firstreplettr.py
+++ firstreplettr.py
@@ -6,9 +6,10 @@ for i in range(0,n):
     l.append(s)
 co = []
 for i in range(0,len(l)):
-    co.append(0)
+    co.append(1)
 for i in range(0,len(l)):
-    co[i] = l.count(l[i])
+    if (l[i] in l[:i]):
+        co[i] = l.count(l[i])
     if co[i] > 1:
         print(l[i])
         exit(1)
