bf5e3024020c76bcf8c3056a73ede17ce0a5b3bf
Ashiscodebat <44222051+Ashiscodebat@users.noreply.github.com>
2018-10-20 19:49:32 +0530

    distinct pairs with minimum sum and no. of pairs

--- distinctpaircount.py
+++ distinctpaircount.py
@@ -1,6 +1,8 @@
 def checkrep(a,b,dic):
     flag = 0
     for key,vals in dic.items():
+        if key + vals > a + b:
+            return 1
         if a != key and b != key:
             flag = 1
         else:
@@ -12,14 +14,15 @@ l= []
 d = {}
 dic = {}
 count = 0
-sum = 47
 an = 0
+minsum = 1000000
 for  i in range(0,n):
     s =int(input())
     l.append(s)
 for i in range(0,n):
     for j in range(i+1,n):
-        if l[i] + l[j] == sum :
+        if l[i] + l[j] < minsum :
+            minsum = l[i] + l[j]
             an = checkrep(l[i],l[j],dic)
             if an == 1:
                 d = {l[i]:l[j]}
@@ -28,4 +31,8 @@ for i in range(0,n):
                 an = 0
             else:
                 continue
+li = []
+for key,val in dic.items():
+    li.extend([key,val])
 print(count)
+print(li)
